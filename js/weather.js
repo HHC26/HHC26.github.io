8ea462ad8ab811aa3d7fcb26f9ee643fe960e90c
HHC26 <214232667@qq.com>
2022-11-04 18:02:50 +0800

    Daily updated：2022-11-04 18:02:50 by GS PC

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "2784dd3fcb1e4f0f9a9b579bf69641f2"
+  }
+}
